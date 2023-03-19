9afde4507b25fba03eb4831a6b90cbc91b1056bb
Anton Shcherbyna <khdevnet@gmail.com>
2023-03-19 15:18:20 +0100

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # curl -s https://raw.githubusercontent.com/khdevnet/wsl2-env/main/install-docker.sh | sh
 # https://dev.solita.fi/2021/12/21/docker-on-wsl2-without-docker-desktop.html
-# https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+# install doc https://docs.docker.com/engine/install/ubuntu/#install-using-the-repository
+# autorun hint https://docs.docker.com/engine/security/rootless/
 
-sudo apt remove docker docker-engine docker.io containerd runc
+sudo apt remove -y docker docker-engine docker.io containerd runc
 
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y \
     ca-certificates \
     curl \
@@ -19,16 +20,20 @@ echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
+sudo apt-get update -y
 
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
-sudo apt-get update
+sudo apt-get update -y
 
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 sudo groupadd docker
 sudo usermod -aG docker $USER
 
+# TEMP
+# sudo apt-get install -y uidmap
+# sudo apt-get install -y dbus-user-session
+
 # start automatically
-sudo systemctl enable docker.service
-sudo systemctl enable containerd.service
+# sudo systemctl enable docker.service
+# sudo systemctl enable containerd.service
