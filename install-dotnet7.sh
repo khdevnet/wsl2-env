4e7ec330811db7b0a6cf141656f968587c11449e
Anton Shcherbyna <khdevnet@gmail.com>
2023-03-21 20:09:51 +0100

    Update install-dotnet7.sh

--- install-dotnet7.sh
+++ install-dotnet7.sh
@@ -3,12 +3,13 @@
 
 # clean
 # https://learn.microsoft.com/en-us/dotnet/core/install/linux-package-mixup?pivots=os-linux-ubuntu#i-need-a-version-of-net-that-isnt-provided-by-my-linux-distribution
-# apt remove 'dotnet*' 'aspnet*' 'netstandard*'
-# touch /etc/apt/preferences
 
-# "Package: dotnet* aspnet* netstandard*" >> /etc/apt/preferences
-# "Pin: origin \"archive.ubuntu.com\"" >> /etc/apt/preferences
-# "Pin-Priority: -10" >> /etc/apt/preferences
+apt remove 'dotnet*' 'aspnet*' 'netstandard*'
+
+touch /etc/apt/preferences
+"Package: dotnet* aspnet* netstandard*" >> /etc/apt/preferences
+"Pin: origin \"archive.ubuntu.com\"" >> /etc/apt/preferences
+"Pin-Priority: -10" >> /etc/apt/preferences
 
 # Get Ubuntu version
 declare repo_version=$(if command -v lsb_release &> /dev/null; then lsb_release -r -s; else grep -oP '(?<=^VERSION_ID=).+' /etc/os-release | tr -d '"'; fi)
