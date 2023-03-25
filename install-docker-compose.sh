7a114b3ec6154a3fa056bc9833bbb4cf1a38ee88
Anton Shcherbyna <khdevnet@gmail.com>
2023-03-25 14:19:36 +0100

    Update install-docker-compose.sh

--- install-docker-compose.sh
+++ install-docker-compose.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+# curl -s https://raw.githubusercontent.com/khdevnet/wsl2-env/main/install-docker-compose.sh | sh
 # https://www.digitalocean.com/community/tutorials/how-to-install-and-use-docker-compose-on-ubuntu-22-04
 
 mkdir -p ~/.docker/cli-plugins/
